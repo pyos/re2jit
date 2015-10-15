e50ddfc790301ea3838a3089dd7122d0c8ed2023
pyos <pyos100500@gmail.com>
2015-10-15 18:21:54 +0300

    Join some tests.

--- test/basic_perf.cc
+++ test/basic_perf.cc
@@ -1,44 +1,36 @@
-test_case("re2 linkage")
+test_case("re2 'Hello, World!'")
 {
     _RE2(regex, "Hello, World!");
     MEASURE(1000000, _RE2_FULL(regex, "Hello, World!"));
 }
 
 
-test_case("re2 regex match")
+test_case("re2 '[hH]ello,? +[Ww]orld(\\?|!|\\.|)'")
 {
     re2::StringPiece xs;
     _RE2(regex, "[hH]ello,? +[Ww]orld(\\?|!|\\.|)");
-    MEASURE(1000000, _RE2_FULL(regex, "Hello, World!", &xs));
+
+    MEASURE(1000000, {
+        _RE2_FULL(regex, "Hello, World!", &xs);
+        _RE2_FULL(regex, "Hello, Worldd", &xs);
+    });
 }
 
 
-test_case("re2 regex non-match")
-{
-    re2::StringPiece xs;
-    _RE2(regex, "[hH]ello,? +[Ww]orld(\\?|!|\\.|)");
-    MEASURE(1000000, _RE2_FULL(regex, "Hello, re2!", &xs));
-}
-
-
-test_case("re2jit linkage")
+test_case("re2jit 'Hello, World!'")
 {
     _R2J(regex, "Hello, World!");
     MEASURE(1000000, _R2J_FULL(regex, "Hello, World!"));
 }
 
 
-test_case("re2jit regex match")
+test_case("re2jit '[hH]ello,? +[Ww]orld(\\?|!|\\.|)'")
 {
     re2::StringPiece xs;
     _R2J(regex, "[hH]ello,? +[Ww]orld(\\?|!|\\.|)");
-    MEASURE(1000000, _R2J_FULL(regex, "Hello, World!", &xs, 1));
-}
 
-
-test_case("re2jit regex non-match")
-{
-    re2::StringPiece xs;
-    _R2J(regex, "[hH]ello,? +[Ww]orld(\\?|!|\\.|)");
-    MEASURE(1000000, _R2J_FULL(regex, "Hello, re2!", &xs, 1));
+    MEASURE(1000000, {
+        _R2J_FULL(regex, "Hello, World!", &xs, 1);
+        _R2J_FULL(regex, "Hello, Worldd", &xs, 1);
+    });
 }
