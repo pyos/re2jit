703f44cb887a77195125d8e92d1d7d766f17a904
pyos <pyos100500@gmail.com>
2015-10-20 18:55:25 +0300

    Add some Unicode tests for which the answer is "no".

--- test/31-unicode.cc
+++ test/31-unicode.cc
@@ -9,6 +9,7 @@ GROUP_PERF_TEST_EX(5000, "(\\p{L}*)", ANCHOR_BOTH, "кириллица", 2);
 GROUP_PERF_TEST_EX(5000, "(\\p{L}*)", ANCHOR_BOTH, "ελληνικά", 2);
 GROUP_PERF_TEST_EX(5000, "(\\p{L}*)", ANCHOR_BOTH, "繁體字", 2);
 GROUP_PERF_TEST_EX(5000, "(\\p{L}*)", ANCHOR_BOTH, "カタカナ", 2);
+GROUP_PERF_TEST_EX(5000, "(\\p{L}*)", ANCHOR_BOTH, "asdfg 1234567890 fail", 2);
 
 GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, "1234567890", 2);
 GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, "ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩⅪⅫⅬⅭⅮⅯ", 2);
@@ -16,3 +17,4 @@ GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, "ⅠⅡⅢⅣⅤⅥⅦⅧⅨ�
 // on numerals in Unicode.
 GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, "ΠΔΗΧΜ", 2);
 GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, ")𐅀𐅁𐅂𐅃𐅄𐅅𐅆𐅇𐅈𐅉𐅊𐅋𐅌𐅍𐅎𐅏", 2);
+GROUP_PERF_TEST_EX(9000, "(\\p{N}*)", ANCHOR_BOTH, "12345 fail 67890", 2);
