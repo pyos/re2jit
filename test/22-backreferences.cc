17e51bcecd7f051a4aa2a90d7476790052f0808d
pyos <pyos100500@gmail.com>
2015-10-22 21:30:00 +0300

    Add a potential buffer overrun test.

--- test/22-backreferences.cc
+++ test/22-backreferences.cc
@@ -3,3 +3,4 @@ FIXED_TEST("(cat|dog|)\\1", ANCHOR_START, "dogdog", true, "dogdog", "dog");
 FIXED_TEST("(cat|dog|)\\1", ANCHOR_START, "snek",   true, "",       "");
 FIXED_TEST("(cat|dog|)\\1", ANCHOR_START, "catdog", false);
 FIXED_TEST("(cat|dog|)\\1", ANCHOR_START, "dogcat", false);
+FIXED_TEST("(cat|dog|)\\1", ANCHOR_START, "cat",    false);
