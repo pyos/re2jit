818aa011d2cfcbba1b52e49913cec2731155d2cf
pyos <pyos100500@gmail.com>
2015-11-06 15:23:18 +0300

    Move gpref-generated stuff to the cc file.
    Also, fix some weird linkage BS when optimizations are off.
    gcc absolutely refuses to emit code for a gnu_inline function
    even though there is an extern definition and everything. Argh.

--- re2jit/unicodedata.py
+++ re2jit/unicodedata.py
@@ -10,10 +10,10 @@ def writeinto(file, data, *args, **kwargs):
         print(textwrap.dedent(data).format(*args, **kwargs), file=fd)
 
 
-def gperf(xs, name, struct, initial):
+def gperf(xs, name, initial):
     return subprocess.check_output(
-        ['gperf', '-N', name, '-H', name + '_hash', '-F', initial, '-ctCEIL', 'ANSI-C', '--null-strings'],
-        input='struct {}_t {{ {} }};\n%%\n'.format(name, struct).encode('utf-8') +
+        ['gperf', '-N', name, '-H', name + '_hash', '-F', initial, '-ctCETL', 'ANSI-C', '--null-strings'],
+        input='struct {}_t;\n%%\n'.format(name).encode('utf-8') +
               '\n'.join(','.join(map(str, x)) for x in xs).encode('utf-8')).decode('utf-8')
 
 
@@ -65,30 +65,54 @@ print("unique blocks: ", len(TABLE_CATEGORY_2) >> BLOCK_SIZE)
 writeinto(os.path.join(os.path.dirname(__file__), 'unicodedata.h'),
     '''
     // make re2jit/unicodedata.h
-    #include <stdint.h>
-    #define UNICODE_2STAGE_GET(t, c) t##_2[(c) % (1 << {1}) + t##_1[(c) >> {1}]]
+    #ifndef RE2JIT_UNICODEDATA_H
+    #define RE2JIT_UNICODEDATA_H
 
-    static const uint32_t UNICODE_SPACE_SIZE = {0};
-    static const uint32_t UNICODE_BLOCK_SIZE = {1};
+    #ifdef __cplusplus
+    extern "C" {{
+    #endif
+        #include <stdint.h>
+        #include <string.h>
+        #define UNICODE_2STAGE_GET(t, c) t##_2[(c) % (1 << {1}) + t##_1[(c) >> {1}]]
 
-    extern const uint32_t UNICODE_CATEGORY_1[];
-    extern const uint8_t  UNICODE_CATEGORY_2[];
-    static const uint8_t  UNICODE_CATEGORY_GENERAL = 0x0F;
-    {2}
+        static const uint32_t UNICODE_SPACE_SIZE = {0};
+        static const uint32_t UNICODE_BLOCK_SIZE = {1};
+        static const uint8_t  UNICODE_CATEGORY_GENERAL = {2};
+        extern const uint32_t UNICODE_CATEGORY_1[];
+        extern const uint8_t  UNICODE_CATEGORY_2[];
+
+        struct _rejit_uni_cat_id_t {{ const char *name; uint8_t id; }};
+        extern const struct
+            _rejit_uni_cat_id_t
+           *_rejit_uni_cat_id(const char *, unsigned int);
+    #ifdef __cplusplus
+    }};
+    #endif
+
+    #endif
     ''',
     SPACE_SIZE,
     BLOCK_SIZE,
-    gperf(TABLE_CATEGORY_N.items(), '_rejit_uni_cat_id', 'const char *name; uint8_t id;', ', 0'),
+    (1 << 4) - 1,
 )
 
 
 writeinto(os.path.join(os.path.dirname(__file__), 'unicodedata.cc'),
     '''
-    #include "unicodedata.h"
+    extern "C" {{
+        #include "unicodedata.h"
+        #include <string.h>
+        // gperf declares the lookup function as gnu_inline but not static.
+        // that still breaks linkage for some reason.
+        #define __inline
+        #define __gnu_inline__
 
-    extern const uint32_t UNICODE_CATEGORY_1[] = {{ {} }};
-    extern const uint8_t  UNICODE_CATEGORY_2[] = {{ {} }};
+        extern const uint32_t UNICODE_CATEGORY_1[] = {{ {} }};
+        extern const uint8_t  UNICODE_CATEGORY_2[] = {{ {} }};
+        {}
+    }};
     ''',
     ','.join(str(x)                   for x in TABLE_CATEGORY_1),
     ','.join(str(TABLE_CATEGORY_N[x]) for x in TABLE_CATEGORY_2),
+    gperf(TABLE_CATEGORY_N.items(), '_rejit_uni_cat_id', ', 0'),
 )
