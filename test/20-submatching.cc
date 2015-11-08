3732d0de9f33f05637b6bea85144afe2ec728ead
pyos <pyos100500@gmail.com>
2015-11-08 15:00:50 +0300

    Fix an invalid comment.
    The last location of a subgroup match is returned, not the first.

--- test/20-submatching.cc
+++ test/20-submatching.cc
@@ -7,9 +7,9 @@ MATCH_TEST("([+-]?(?:(0b)[01]+|(0o)[0-7]+|(0x)[0-9a-f]+|[0-9]+((\\.[0-9]+)?(e[+-
 MATCH_TEST("([+-]?(?:(0b)[01]+|(0o)[0-7]+|(0x)[0-9a-f]+|[0-9]+((\\.[0-9]+)?(e[+-]?[0-9]+)?)(j)?))", ANCHOR_BOTH, "30.5e+1j", 9);
 
 // When a group A matches multiple times, the entry for each subgroup it contains
-// should record the first location that subgroup matched at, even if these locations
+// should record the last location that subgroup matched at, even if these locations
 // are within different matches of group A. For example, in the following regexps
-// group `(\s+)` matches ' ' and group `(\w+)` matches 'submatch', even though
-// their common parent group matches `submatch`.
+// group `(\s+)` matches ' ' and group `(\w+)` matches 'test', even though
+// their common parent group matches 'test' (without the space).
 MATCH_TEST("((\\s+)|(\\w+))+",   ANCHOR_START, "submatch test", 4);
 MATCH_TEST("(?:(\\s+)|(\\w+))+", ANCHOR_START, "submatch test", 4);
